e4e3e43b91cd6664e50ea45dd7991b1611a16175
okikiola94 <farominiyiopeyemi@gmail.com>
2022-11-03 23:03:58 +0100

    Update

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -20,10 +20,7 @@ const Navbar = (props) => {
         <img src={`${process.env.PUBLIC_URL}/assets/images/blue-dark-logo.png`}  alt="logo" />
       </a>
 
-      <button className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}>
+      <button className="hamburger">
         {/* {!isNavExpanded ?   <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
           width="100%" height="100%"
           viewBox="0 0 20 20"
@@ -34,19 +31,19 @@ const Navbar = (props) => {
            <hr className='line'/>
       </button>
 
-      <button className="hamburger1"
+      {/* <button className="hamburger1"
         onClick={() => {
           setIsNavExpanded(!isNavExpanded);
-        }}>
+        }}> */}
         {/* {!isNavExpanded ?   <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
           width="100%" height="100%"
           viewBox="0 0 20 20"
           fill="white">
           <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"></path></svg>: */}
-           <hr className='line2'/> 
+           {/* <hr className='line2'/> 
            <hr className='line2'/>
            <hr className='line2'/>
-      </button>
+      </button> */}
     
          
       {/* 
