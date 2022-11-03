3cceffc345c8ee6218a7f9bf9bc11ef3c0be8145
okikiola94 <farominiyiopeyemi@gmail.com>
2022-11-03 23:09:24 +0100

    Update

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -20,7 +20,9 @@ const Navbar = (props) => {
         <img src={`${process.env.PUBLIC_URL}/assets/images/blue-dark-logo.png`}  alt="logo" />
       </a>
 
-      <button className="hamburger">
+      <button className="hamburger" onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
         {/* {!isNavExpanded ?   <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
           width="100%" height="100%"
           viewBox="0 0 20 20"
